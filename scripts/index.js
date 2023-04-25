a6268045247e75c4db05e15426bf5341ff51bd6a
Anastasia Odintsova <Barsurku@yandex.ru>
2023-04-25 20:48:28 +0300

    fixes 2

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,15 @@ const editButton = document.querySelector(".profile__edit-button");
 const editButtonPopup = document.querySelector(".popup_type_edit-button");
 const editButtonPopupCloseButton = editButtonPopup.querySelector(".popup__close");
 const nameInput = editButtonPopup.querySelector(".popup__input_type_name");
-const nameProfile = document.querySelector(".profile__title_popup");
+const nameProfile = document.querySelector(".profile__title");
 const infoInput = editButtonPopup.querySelector(".popup__input_type_info");
-const infoProfile = document.querySelector(".profile__subtitle_popup");
+const infoProfile = document.querySelector(".profile__subtitle");
 const editButtonPopupForm = editButtonPopup.querySelector(".popup__form");
 
 
 
 //Отрытие попапа
 editButton.addEventListener('click', () => {
-    console.log("click");
-
     editButtonPopup.classList.add("popup_opened");
 
     nameInput.value = nameProfile.textContent;
@@ -20,7 +18,7 @@ editButton.addEventListener('click', () => {
 });
 
 //закрытие попапа нажатием на крестик
-editButtonPopupCloseButton.addEventListener('click', () => {
+    editButtonPopupCloseButton.addEventListener('click', () => {
     editButtonPopup.classList.remove("popup_opened");
 });
 
